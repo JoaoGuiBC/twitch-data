bbcc30820f1eaa5363bdea3ec4b0dd0f9a85c623
JoaoGuiBC <joaoguibc@gmail.com>
2021-12-21 02:37:45 -0300

    Initial commit

---
+++ src/routes.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { AnimatePresence } from 'framer-motion';
+import { useTheme } from 'styled-components';
+import { View } from 'react-native';
+
+import { useAuth } from './hooks/useAuth';
+
+import { SignIn } from './screens/SignIn';
+import { Home } from './screens/Home';
+
+export function Routes() {
+  const theme = useTheme();
+
+  const { user } = useAuth();
+
+  return (
+    <View style={{ backgroundColor: theme.colors.black, flex: 1 }}>
+      <AnimatePresence exitBeforeEnter>
+        {user.id ? <Home key="home" /> : <SignIn key="signIn" />}
+      </AnimatePresence>
+    </View>
+  );
+}
